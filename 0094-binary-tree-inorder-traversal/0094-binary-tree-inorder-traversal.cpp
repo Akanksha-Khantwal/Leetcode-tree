86431fde367f1e04bba9d52153eec5e2673bf29f
Akanksha-Khantwal <akankshakhantwal737@gmail.com>
2025-11-11 18:48:14 +0530

    Time: 0 ms (100%), Space: 10.9 MB (32.95%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        while(root){
+            if(!root->left){
+                ans.push_back(root->val);
+                root=root->right;
+            }
+            else{
+                TreeNode* curr=root->left;
+                while(curr->right &&curr->right!=root)
+                curr=curr->right;
+                if(curr->right==NULL){
+                    curr->right=root;
+                    root=root->left;
+                }
+                else{
+                    curr->right=NULL;
+                    ans.push_back(root->val);
+                    root=root->right;
+                }
+
+            }
+        }
+        return ans;
+      
+    }
+};
